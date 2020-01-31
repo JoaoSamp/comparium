0ed8d028e8a7e5c97acb7ce9f06063ac26a3999a
Joao Sampaio Dos Santos <joao.ssantos@ipiranga.com.br>
2020-01-31 14:38:14 -0300

    parsed

--- obj_parser.js
+++ obj_parser.js
@@ -4,7 +4,6 @@ class Obj_Parser{
         this.name           = name || 'default_name'
         this.obj            = obj
         this.parsed_obj     = {}
-        this.xml_obj
         this.ignored_words  = ignored_words
         this.non_defined    = non_defined
         this.key_words      = key_words
@@ -21,18 +20,20 @@ class Obj_Parser{
     }
 
     Is_Key_Word( word ) {
-        if ( this.key_words[word] ){
-            return true
-        } 
+        if ( !this.ignored_words.includes(word) ){
+            if ( this.key_words[word] ){
+                return true
+            } 
+        }
         return false        
     }
-
-    Is_Non_Defined( word ) {
-        return this.non_defined.includes(word)    
-    }
     
-    Is_Ignored( word){
-        return this.ignored_words.includes(word)  
+    Add_If_Non_Defined( word){
+        if ( !this.ignored_words.includes(word) ){
+            if ( !this.non_defined.includes(word) ){
+                this.non_defined.push(word)
+            } 
+        }
     }
 
     Create_Key_Obj(obj, key_name){
@@ -62,29 +63,22 @@ class Obj_Parser{
             return this.Create_Key_Obj(obj, key_name)
         }
     }
-
-    Run_Through_Obj (obj, par_obj){
-        let parent_obj = par_obj || this.parsed_obj
+    
+    Run_Through_Obj (obj){
+        
         if (Array.isArray(obj)){
             obj.map((item) => {
-                this.Run_Through_Obj(item, parent_obj);
+                this.Run_Through_Obj(item);
             })
         }else if (typeof obj === 'object'){
             for (let prop in obj){
                 if ( this.Is_Key_Word(prop) ){
-                    parent_obj[prop] = this.Parse_Key_Obj(obj[prop], this.key_words[prop])
-                } else if (this.Is_Ignored(prop)){
-                    if (!parent_obj[prop]){
-                        parent_obj[prop] = {}    
-                    }
-                    this.Run_Through_Obj(obj[prop], obj)
-                }else if(this.Is_Non_Defined(prop)){
-                    this.non_defined.push(prop)
-                    this.Run_Through_Obj(obj[prop], obj)
+                    this.parsed_obj[prop] = this.Parse_Key_Obj(obj[prop], this.key_words[prop])
+                } else {
+                    this.Add_If_Non_Defined(prop)
+                    this.Run_Through_Obj(obj[prop])
                 }
             }
-        }else{
-            console.log('else')
         }
     }
 }
